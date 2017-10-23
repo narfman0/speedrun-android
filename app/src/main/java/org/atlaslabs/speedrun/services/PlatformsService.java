bd1582ef6910dbe150e7aa3026a2a0990b3e7c9b
Jon Robison <narfman0@gmail.com>
2017-10-22 20:57:51 -0400

    -Make this service more simple. It is already on a background service; make sure everything is async and use single realm instance. More performant and simple code==win win win

--- app/src/main/java/org/atlaslabs/speedrun/services/PlatformsService.java
+++ app/src/main/java/org/atlaslabs/speedrun/services/PlatformsService.java
@@ -9,7 +9,6 @@ import org.atlaslabs.speedrun.network.RestUtil;
 
 import java.util.Arrays;
 
-import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
 
 public class PlatformsService extends IntentService {
@@ -21,33 +20,21 @@ public class PlatformsService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent workIntent) {
-        long totalPlatforms = 0;
         Realm realm = Realm.getDefaultInstance();
         try {
-            totalPlatforms = realm.where(Platform.class).count();
+            long totalPlatforms = realm.where(Platform.class).count();
+            if(totalPlatforms == 0)
+                RestUtil.createAPI().getPlatformsBulk().subscribe((item) -> {
+                    realm.beginTransaction();
+                    realm.copyToRealmOrUpdate(Arrays.asList(item.getPlatforms()));
+                    realm.commitTransaction();
+                });
         } finally {
             realm.close();
         }
-        if(totalPlatforms == 0)
-            populate();
-    }
 
-    private void populate(){
-        RestUtil.createAPI().getPlatformsBulk()
-                .subscribeOn(Schedulers.newThread())
-                .subscribe((item) -> {
-                    Realm realm = Realm.getDefaultInstance();
-                    try {
-                        realm.beginTransaction();
-                        realm.copyToRealmOrUpdate(Arrays.asList(item.getPlatforms()));
-                        realm.commitTransaction();
-                    } finally {
-                        realm.close();
-                    }
-
-                    // Broadcast that we are done populating game data
-                    LocalBroadcastManager.getInstance(this).sendBroadcast(
-                            new Intent(INTENT_PLATFORMS_COMPLETE));
-                });
+        // Broadcast that we are done populating game data
+        LocalBroadcastManager.getInstance(this).sendBroadcast(
+                new Intent(INTENT_PLATFORMS_COMPLETE));
     }
 }
